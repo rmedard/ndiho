771d3c772fa044c4befdfe4df294b0ef737ec7ce
Rebero <rmedard5@gmail.com>
2022-12-28 03:46:41 +0100

    fix

--- web/modules/ndiho_interface/src/Controller/NdihoInterfaceController.php
+++ web/modules/ndiho_interface/src/Controller/NdihoInterfaceController.php
@@ -2,7 +2,11 @@
 
 namespace Drupal\ndiho_interface\Controller;
 
+use Drupal;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Menu\MenuTreeParameters;
+use Drupal\Core\Routing\RouteProvider;
+use Drupal\Core\Url;
 
 /**
  * Returns responses for Ndiho Interface routes.
@@ -12,6 +16,40 @@ class NdihoInterfaceController extends ControllerBase {
   /**
    * Builds the response.
    */
+
+  public function settingsPage(): array {
+    $routeProvider = Drupal::service('router.route_provider');
+    $content = [];
+
+    $parameters = new MenuTreeParameters();
+    $elements = Drupal::menuTree()->load('main', $parameters);
+    foreach ($elements as $element) {
+      Drupal::logger('test')->info($element->link->getTitle());
+    }
+
+
+    if ($routeProvider instanceof RouteProvider) {
+      $socialMedia = $routeProvider->getRouteByName('ndiho_interface.social_media.settings');
+      $donations = $routeProvider->getRouteByName('ndiho_interface.donation.settings');
+      $content[] = [
+        'title' => $socialMedia->getDefault('_title'),
+        'url' => Url::fromUri('internal:' . $socialMedia->getPath()),
+        'description' => $socialMedia->getDefault('_description')
+      ];
+      $content[] = [
+        'title' => $donations->getDefault('_title'),
+        'url' => Url::fromUri('internal:' . $donations->getPath()),
+        'description' => $donations->getDefault('_description')
+      ];
+    }
+
+    $output['ndiho_setting_links'] = [
+      '#theme' => 'admin_block_content',
+      '#content' => $content,
+    ];
+    return $output;
+  }
+
   public function homepage(): array {
     return ['#theme' => 'homepage'];
   }
--- web/themes/custom/ndiho_theme/js/custom.js
+++ web/themes/custom/ndiho_theme/js/custom.js
@@ -3,137 +3,138 @@
  * Global utilities.
  *
  */
-(function () {
+(function (Drupal, once) {
 
   'use strict';
 
   Drupal.behaviors.ndiho_theme = {
     attach: function (context, settings) {
+      initAos();
+      initDropDownMenus();
 
-      AOS.init({
-        // Global settings:
-        disable: false, // accepts following values: 'phone', 'tablet',
-                        // 'mobile', boolean, expression or function
-        startEvent: 'DOMContentLoaded', // name of the event dispatched on the
-                                        // document, that AOS should initialize
-                                        // on
-        initClassName: 'aos-init', // class applied after initialization
-        animatedClassName: 'aos-animate', // class applied on animation
-        useClassNames: false, // if true, will add content of `data-aos` as
-                              // classes on scroll
-        disableMutationObserver: false, // disables automatic mutations'
-                                        // detections (advanced)
-        debounceDelay: 50, // the delay on debounce used while resizing window
-                           // (advanced)
-        throttleDelay: 99, // the delay on throttle used while scrolling the
-                           // page (advanced)
+      const form = once.find('donation-form', context)[0];
+      if (form !== undefined) {
+        console.log(form.localName);
+        const donationButton = once.find('submit-donation-btn', form).at(0);
+        const donationInput = once.find('donation-amount', form).at(0);
+        let donation = donationInput.value;
+        const freeDonationField = once.find('free-donation-field', form).at(0);
+        const radios = once('fetch_radios', 'input[type=radio]', form);
+        radios.forEach(function (radio) {
+          radio.addEventListener('change', function (event) {
+            const checkedInput = event.currentTarget;
+            if (checkedInput instanceof Element) {
+              const checkedLabel = form.querySelector(`label[for="${checkedInput.id}"]`);
+              if (checkedInput.checked) {
+                donation = parseInt(checkedInput.value);
+                checkedLabel.classList.add('btn-dark');
+                checkedLabel.classList.remove('btn-outline-dark');
+                if (checkedInput.value === 'other') {
+                  donationInput.value = 0;
+                  freeDonationField.removeAttribute('hidden');
+                  freeDonationField.querySelector('input').focus();
+                }
+                else {
+                  freeDonationField.setAttribute('hidden', 'true');
+                }
+              }
 
-        // Settings that can be overridden on per-element basis, by
-        // `data-aos-*` attributes:
-        offset: 120, // offset (in px) from the original trigger point
-        delay: 0, // values from 0 to 3000, with step 50ms
-        duration: 400, // values from 0 to 3000, with step 50ms
-        easing: 'ease-in', // default easing for AOS animations
-        once: true, // whether animation should happen only once - while
-                    // scrolling down
-        mirror: false, // whether elements should animate out while scrolling
-                       // past them
-        anchorPlacement: 'top-bottom', // defines which position of the element
-                                       // regarding to window should trigger
-                                       // the animation
-      });
-
-      /**
-       * Donation form
-       */
-
-      const form = document.getElementById('donation-form');
-      if (form) {
-        let donation = 0;
-        const freeDonationField = form.querySelector('div.free-donation-field');
-        const donateSubmitBtn = form.querySelector('button#donate-submit-btn');
-        form.querySelectorAll('input[type=radio]').forEach(radio => {
-          if (radio.hasAttribute('checked')) {
-            donation = parseInt(radio.value);
-          }
-          radio.addEventListener('change', function (value) {
-            let selectedValue = radio.value;
-            form.querySelectorAll('label').forEach((el) => {
-              el.classList.remove('btn-dark');
-              el.classList.add('btn-outline-dark');
-            });
-            const selectedInput = form.querySelector(`input[value="${selectedValue}"]`);
-            const selectedInputLabel = form.querySelector(`label[for="${selectedInput.id}"]`)
-            selectedInputLabel.classList.remove('btn-outline-dark');
-            selectedInputLabel.classList.add('btn-dark');
-            if (selectedValue === 'other') {
-              donation = 0;
-              freeDonationField.removeAttribute('hidden');
-              freeDonationField.querySelector('input').focus();
+              radios
+                .filter(radio => radio.id !== checkedInput.id)
+                .forEach(uncheckedRadio => {
+                  if (uncheckedRadio.id !== checkedInput.id) {
+                    const uncheckedLabel = form.querySelector(`label[for="${uncheckedRadio.id}"]`);
+                    uncheckedLabel.classList.remove('btn-dark');
+                    uncheckedLabel.classList.add('btn-outline-dark');
+                  }
+                });
             }
-            else {
-              freeDonationField.setAttribute('hidden', "true");
-              donation = parseInt(selectedValue);
-            }
-          })
+          });
         });
-        freeDonationField.querySelector('input').addEventListener('keyup', function () {
+        donationInput.addEventListener('keyup', function () {
           donation = parseInt(this.value);
         });
-        donateSubmitBtn.addEventListener('click', function () {
+        donationButton.addEventListener('click', function (e) {
           if (donation > 0) {
             window.open('https://www.paypal.com/paypalme/rebero/' + donation);
           }
         });
       }
+    }
+  };
 
-      /**
-       * End of donation form
-       */
+  function initAos() {
+    AOS.init({
+      // Global settings:
+      disable: false, // accepts following values: 'phone', 'tablet',
+                      // 'mobile', boolean, expression or function
+      startEvent: 'DOMContentLoaded', // name of the event dispatched on the
+                                      // document, that AOS should initialize
+                                      // on
+      initClassName: 'aos-init', // class applied after initialization
+      animatedClassName: 'aos-animate', // class applied on animation
+      useClassNames: false, // if true, will add content of `data-aos` as
+                            // classes on scroll
+      disableMutationObserver: false, // disables automatic mutations'
+                                      // detections (advanced)
+      debounceDelay: 50, // the delay on debounce used while resizing window
+                         // (advanced)
+      throttleDelay: 99, // the delay on throttle used while scrolling the
+                         // page (advanced)
 
-      /**
-       * Dropdown menu
-       */
-      const dropdownElementList = document.querySelectorAll('.dropdown-toggle');
-      dropdownElementList.forEach((menu) => {
-        const id = Math.random().toString(36).slice(2, 7);
-        menu.setAttribute('id', `drop-down-${id}`);
-        let dropdownMenu = menu.parentElement.querySelector('ul.dropdown-menu');
-        dropdownMenu.setAttribute('aria-labelledby', `drop-down-${id}`);
-        let dropdown = new mdb.Dropdown(menu);
-        let itemHovered = false;
-        let menuHovered = false;
-        const cursorTimeout = 100;
-        menu.addEventListener('mouseover', function () {
-          menuHovered = true;
-          dropdown.show();
+      // Settings that can be overridden on per-element basis, by
+      // `data-aos-*` attributes:
+      offset: 120, // offset (in px) from the original trigger point
+      delay: 0, // values from 0 to 3000, with step 50ms
+      duration: 400, // values from 0 to 3000, with step 50ms
+      easing: 'ease-in', // default easing for AOS animations
+      once: true, // whether animation should happen only once - while
+                  // scrolling down
+      mirror: false, // whether elements should animate out while scrolling
+                     // past them
+      anchorPlacement: 'top-bottom', // defines which position of the element
+                                     // regarding to window should trigger
+                                     // the animation
+    });
+  }
+
+  function initDropDownMenus() {
+    const dropdownElementList = document.querySelectorAll('.dropdown-toggle');
+    dropdownElementList.forEach((menu) => {
+      const id = Math.random().toString(36).slice(2, 7);
+      menu.setAttribute('id', `drop-down-${id}`);
+      let dropdownMenu = menu.parentElement.querySelector('ul.dropdown-menu');
+      dropdownMenu.setAttribute('aria-labelledby', `drop-down-${id}`);
+      let dropdown = new mdb.Dropdown(menu);
+      let itemHovered = false;
+      let menuHovered = false;
+      const cursorTimeout = 100;
+      menu.addEventListener('mouseover', function () {
+        menuHovered = true;
+        dropdown.show();
+      });
+      menu.addEventListener('mouseleave', function () {
+        menuHovered = false;
+        setTimeout(function () {
+          if (itemHovered === false && menuHovered === false) {
+            dropdown.hide();
+          }
+        }, cursorTimeout);
+      });
+      dropdownMenu.querySelectorAll('li').forEach(item => {
+        item.addEventListener('mouseover', function () {
+          itemHovered = true;
         });
-        menu.addEventListener('mouseleave', function () {
-          menuHovered = false;
+        item.addEventListener('mouseleave', function () {
+          itemHovered = false;
           setTimeout(function () {
             if (itemHovered === false && menuHovered === false) {
               dropdown.hide();
             }
           }, cursorTimeout);
         });
-        dropdownMenu.querySelectorAll('li').forEach(item => {
-          item.addEventListener('mouseover', function () {
-            itemHovered = true;
-          });
-          item.addEventListener('mouseleave', function () {
-            itemHovered = false;
-            setTimeout(function () {
-              if (itemHovered === false && menuHovered === false) {
-                dropdown.hide();
-              }
-            }, cursorTimeout);
-          });
-        });
       });
-      /**
-       * End of dropdown menu
-       */
-    }
-  };
+    });
+  }
 
-})();
+}(Drupal, once));
